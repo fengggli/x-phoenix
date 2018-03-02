d1ffddddbaabfe2e318b9d9e9ca4e342e003c0d4
Feng Li <li2251@purdue.edu>
2018-03-02 10:11:16 -0500

    confirm the error with test case

--- tests/test_alloc/test_alloc.cpp
+++ tests/test_alloc/test_alloc.cpp
@@ -9,6 +9,10 @@
  * e-mail: fengggli@yahoo.com
  */
 
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <mcheck.h>
 #include <gtest/gtest.h>
 
@@ -17,10 +21,43 @@
 #include <vector>
 
 #include "allocator_chooser.h"
+#include "stddefines.h"
 
 
 using namespace std;
 
+/*
+ * test basic use of allocator
+ */
+TEST(AllocatorTest, ByteRead){
+    
+    ALLOCATOR<char> alloc;
+    int fd;
+    struct stat finfo;
+    int ret;
+
+    char *fname = "/home/lifeng/phoenix/phoenix++-1.0/data/histogram_datafiles/XING_B24.BMP";
+    CHECK_ERROR((fd = open(fname, O_RDONLY)) < 0);
+    // Get the file info (for file length)
+    CHECK_ERROR(fstat(fd, &finfo) < 0);
+
+
+
+    auto const p= alloc.allocate(4096*20); // 20 pages 
+    
+
+    memset(p, 0x0, 20*4096);
+    ret = read (fd, p, 2048);// finfo.st_size);
+    printf("ret = %d, finfo size = %ld\n", ret, finfo.st_size);
+    CHECK_ERROR (ret != finfo.st_size);
+
+    CHECK_ERROR (close (fd) < 0);
+
+    //alloc.construct(q++); // empty constructor
+    alloc.deallocate(p, 5);
+}
+
+#if 0
 /*
  * test basic use of allocator
  */
@@ -77,6 +114,7 @@ TEST(AllocatorTest, Vector){
 }
 
 
+#endif
 
 int main(int argc, char *argv[]){
     //mtrace();
